0f0ce1bb64cbc8d0f619413f9d9c1a120bbe7341
Lee Su Bin <ccomangi2@gmail.com>
2021-04-02 17:04:59 +0900

    마이페이지화면 프로토타입 구현

--- app/src/main/java/com/example/a_ha_main/MypageActivity.kt
+++ app/src/main/java/com/example/a_ha_main/MypageActivity.kt
@@ -1,14 +1,11 @@
 package com.example.a_ha_main
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class LoginActivity : AppCompatActivity() {
+class MypageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
+        setContentView(R.layout.activity_mypage)
     }
 }
