205f4623c1026325bc7315e630848288fdd4acbb
Lee Su Bin <ccomangi2@gmail.com>
2021-04-05 14:52:48 +0900

    로그인 화면 변수 선언

--- app/src/main/java/com/example/a_ha_main/LoginActivity.kt
+++ app/src/main/java/com/example/a_ha_main/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.a_ha_main
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
@@ -10,7 +11,12 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        var edit_name = findViewById(R.id.edit_name) as EditText // 아이디
+        var edit_password = findViewById(R.id.edit_password) as EditText // 비밀번호
+
+        var login_btn = findViewById(R.id.login_btn) as Button // 로그인 버튼
         var join_btn = findViewById(R.id.join_btn) as Button // 회원가입 버튼
+        var password_ok_btn = findViewById(R.id.password_ok_btn) as Button // 비밀번호 찾기 버튼
 
         join_btn.setOnClickListener() {
             val nextIntent = Intent(this, JoinActivity::class.java)
