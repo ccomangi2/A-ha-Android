e6bd409944563038519e6c7cd3564fa957ee2314
Lee Su Bin <ccomangi2@gmail.com>
2021-04-05 15:00:56 +0900

    마이페이지 화면 변수 선언

--- app/src/main/java/com/example/a_ha_main/MypageActivity.kt
+++ app/src/main/java/com/example/a_ha_main/MypageActivity.kt
@@ -4,9 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.RelativeLayout
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class MypageActivity : AppCompatActivity() {
@@ -23,6 +21,11 @@ class MypageActivity : AppCompatActivity() {
         var logout_btn = findViewById(R.id.logout_btn) as Button // 로그아웃 버튼
         var out_btn = findViewById(R.id.out_btn) as Button // 회원탈퇴 버튼
 
+        var proflie_setting_btn = findViewById(R.id.proflie_setting_btn) as Button // 프로필 사진 변경 버튼
+        var proflie = findViewById(R.id.proflie) as ImageView // 프로필 사진
+        var name_textview = findViewById(R.id.name_textview) as TextView // 이름
+        var id_textview = findViewById(R.id.id_textview) as TextView // 아이디
+        var email_textview = findViewById(R.id.email_textview) as TextView // 이메일
 
         menu_btn.setOnClickListener {
             if (menu_view.visibility == View.GONE) {
