5cf08786e4ab908b52e4bb7a4d3d9f2202b1b32a
Lee Su Bin <ccomangi2@gmail.com>
2021-04-05 14:57:59 +0900

    메모작성 화면 변수 선언

--- app/src/main/java/com/example/a_ha_main/MemoWriteActivity.kt
+++ app/src/main/java/com/example/a_ha_main/MemoWriteActivity.kt
@@ -2,6 +2,7 @@ package com.example.a_ha_main
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,7 +11,12 @@ class MemoWriteActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_memowrite)
 
+        var memo_title_edit = findViewById(R.id.memo_title_edit) as EditText // 메모 제목 버튼
+        var memo_edit = findViewById(R.id.memo_edit) as EditText // 메모 내용 버튼
+
         var cancle_btn = findViewById(R.id.cancle_btn) as ImageButton // 취소 버튼
+        var ok_btn = findViewById(R.id.ok_btn) as ImageButton // 작성 버튼
+        var add_btn = findViewById(R.id.add_btn) as ImageButton // 이미지 추가 버튼
 
         cancle_btn.setOnClickListener() {
             val nextIntent = Intent(this, MainActivity::class.java) // 수정 필요
